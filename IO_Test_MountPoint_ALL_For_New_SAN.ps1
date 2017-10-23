2b082ba3388a7c2374259f678c4c4c142eb636c8
Can KAYA <cankaya07@gmail.com>
2017-10-23 15:57:05 +0300

    .

--- IO_Test_MountPoint_ALL_For_New_SAN.ps1
+++ IO_Test_MountPoint_ALL_For_New_SAN.ps1
@@ -5,26 +5,48 @@ $FreePerc = @{Name="Free(%)";expression={[math]::round(((($_.FreeSpace / 1073741
 $volumes = Get-WmiObject  win32_volume | Where-object {$_.SystemVolume -eq $false}
 $a = $volumes | Select  Name,$Capacity, $FreespaceGB, $FreePerc  
 $Thread = "-t"+(((get-counter "\Processor(*)\% idle time").countersamples | select instancename).length -1)
-$OutstandingIO = "-o32"
+$OutstandingIO = "-o16"
 $CapacityParameter = "-c20G"
-$Time = "-d30"
-$NumberOfTests = 64
+$Time = "-d5"
+$NumberOfTests = 2
 $blocksize = 8,64
 $writeRead =0,100
-$HowManyTimesWillTest=2
+$HowManyTimesWillTest=1
 $IOTest="si","r"
+$diskspdExe="C:\Diskspd\amd64fre"
+ 
+ # Copy DiskSpd into current folder 
+if ([System.IO.File]::Exists($diskspdExe + '\diskspd.exe')) {
+	if(!([System.IO.File]::Exists((Resolve-Path .\).Path+'\diskspd.exe'))){
+		Write-Host "Copying diskspd to local directory - $diskspdexe\diskspd.exe"
+		Copy-Item "$diskspdExe\diskspd.exe", (Resolve-Path .\).Path
+	}
+}
+	 
+if ($PSVersionTable.PSVersion.Major -lt 3) {
+    Write-host "Error: This script requires Powershell Version 3 and above to run correctly" -ForegroundColor Red
+	return;
+}
 
+Write-Host "We found these Drives in your system";
+foreach($vol in $a)
+{
+	Write-Host "Will perform this drive" $Disk = $vol.Name
+}
+
+
+
+
+Write-Host "It will take" ($a.count*$HowManyTimesWillTest*($Time.Replace("-d",""))) "second(s)"
 
 write-host "TEST RESULTS (also logged in .\output.txt)" -foregroundcolor yellow
 
 "Test N#, Drive, Operation, Access, Blocks, Run N#, IOPS, MB/sec, Latency ms, CPU %" >> ./output.txt
 foreach($vol in $a)
 {
-	$Disk = $vol.Name
+	$Disk = $vol.Name.Substring(0,$vol.Name.Length-1)
 	# Reset test counter
-	$counter = 0
-	# Use 1 thread / core
-    
+	$counter = 0 
 
 	$IsDir = test-path -path "$Disk\TestDiskSpd"
 	if ($IsDir -like "False")
@@ -33,7 +55,7 @@ foreach($vol in $a)
 	}
 
 	# Just a little security, in case we are working on a compressed drive ...
-	compact /u /s $Disk\TestDiskSpd\
+	#compact /u /s $Disk\TestDiskSpd\
 
 
 	$Cleaning = test-path -path "$Disk\TestDiskSpd\testfile.dat"
@@ -74,11 +96,17 @@ foreach($vol in $a)
 				}
 
 				$AccessParameter = "-"+$_
-				for($counter; $counter -lt $HowManyTimesWillTest;$counter++)
-				{
+				(1..4) | % {
 					# The test itself (finally !!)
+	
+
+	
 					$result = .\diskspd.exe $CapacityParameter $Time $AccessParameter $WriteParameter $Thread $OutstandingIO $BlockParameter -h -L $Disk\TestDiskSpd\testfile.dat
 	 
+
+
+
+
 					# Now we will break the very verbose output of DiskSpd in a single line with the most important values
 					foreach ($line in $result) {if ($line -like "total:*") { $total=$line; break } }
 					foreach ($line in $result) {if ($line -like "avg.*") { $avg=$line; break } }
@@ -89,7 +117,7 @@ foreach($vol in $a)
 					$counter = $counter + 1
 	
 					# A progress bar, for the fun
-					Write-Progress -Activity ".\diskspd.exe $CapacityParameter $Time $AccessParameter $WriteParameter $Thread $OutstandingIO $BlockParameter -h -L $Disk\TestDiskSpd\testfile.dat" -status "Test in progress" -percentComplete ($counter / $NumberofTests * 100)
+					#Write-Progress -Activity ".\diskspd.exe $CapacityParameter $Time $AccessParameter $WriteParameter $Thread $OutstandingIO $BlockParameter -h -L $Disk\TestDiskSpd\testfile.dat" -status "Test in progress" -percentComplete ($counter / $NumberofTests * 100)
 					"Test $Counter,$Disk,$IO,$type,$Blocks,Run $_,$iops,$mbps,$latency,$cpu"  >> ./output.txt
 
 					# We output a verbose format on screen
@@ -99,6 +127,3 @@ foreach($vol in $a)
 		}
 	}
 }
-
-
- 
